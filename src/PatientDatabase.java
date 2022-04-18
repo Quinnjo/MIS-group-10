1248511354f164027a956f8d4994bd0db45bee54
Quinnjo <qm132002@gmail.com>
2022-04-18 16:11:10 -0400

    Update PatientDatabase.java

--- src/PatientDatabase.java
+++ src/PatientDatabase.java
@@ -1,37 +1,74 @@
 /*
  * The patient database class
  */
-static final int MAX_PATIENT_NUMBER = 100;
+import java.util.ArrayList;
 
 public class PatientDatabase {
-    private Patient array[MAX_PATIENT_NUMBER];
+    final int MAX_PATIENT_NUMBER = 100;
+
+    private ArrayList<Patient> patientList;
     private int numPatients;
     String filePath;
 
     public PatientDatabase(String filePath) {
         this.filePath = filePath;
+        patientList = new ArrayList<Patient>(); // make
 
         // TODO: open and process the file
     }
 
     public void insertProfile(Patient patient) {
-        // TODO: insert patient into database
+        // insert patient into database
+        patientList.add(patient);
     }
 
     public void deleteProfile(String lastName, String dateOfBirth) {
-        // TODO: delete patient profile from database
+        // delete patient profile from database
+        for(int i = 0; i < patientList.size(); i++) {
+            Patient p = patientList.get(i);
+            if(p.lastName == lastName && p.dateOfBirth == dateOfBirth) {
+                patientList.remove(i);
+                return;
+            }
+        }
     }
 
     // This method should take another parameter that holds the updated patient information
     // But what type should that parameter be? Another patient class?
     // Remember: a patient's date of birth may not be modified!
-    public void updateProfile(String lastName, String dateOfBirth) {
+    public void updateProfile(String lastName, String dateOfBirth, Patient newPatient) {
         // TODO: update patient profile
+        // TODO: make methods to update specific patient attributes?
+        for(int i = 0; i < patientList.size(); i++) {
+            Patient p = patientList.get(i);
+            if(p.lastName == lastName && p.dateOfBirth == dateOfBirth) {
+                if(dateOfBirth == newPatient.dateOfBirth) {
+                    patientList.set(i, newPatient);
+                    return;
+                } else {
+                    // you can't update a patient's date of birth!
+                    // we shouldn't modify the patient
+                    // should we raise an exception?
+                }
+            }
+        }
+        // if we reach here, no patient was found
+        // should we raise an exception?
     }
 
-    public void displayPatient(String lastName, String dateOfBirth) {
+    public Patient findPatient(String lastName, String dateOfBirth) {
         // TODO: find patient based off of parameters, and display patient
         // What does display mean? What is the return type of this function?
+        // For now, this method returns the patient that we seek
+        for(int i = 0; i < patientList.size(); i++) {
+            Patient p = patientList.get(i);
+            if(p.lastName == lastName && p.dateOfBirth == dateOfBirth) {
+                return patientList.get(i);
+            }
+        }
+        // we couldn't find a patient
+        // return null? raise an exception?
+        return null;
     }
 
     /*
